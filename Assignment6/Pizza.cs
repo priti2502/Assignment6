29d254559c5377366de564ff5fa6157ce1309aeb
Priti Gaikwad <pritisgaikwad25@gmail.com>
2024-07-30 15:02:00 +0530

    Add project files.

---
+++ Assignment6/Pizza.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    internal class Pizza
+    {
+        private string Size { get; set; } 
+        private int NoOfCheeseToppings { get; set; }
+        private int NoOfPepperoniToppings { get; set; }
+        private int NoOfHamToppings { get; set; }
+
+        public Pizza(string size,int cheese,int pepporoni,int ham)
+        {
+            Size= size; 
+            NoOfCheeseToppings= cheese; 
+            NoOfPepperoniToppings = pepporoni;
+            NoOfHamToppings = ham; 
+        
+        }
+       
+        public double calcCost(string Size)
+        {
+            
+            double cost = 0.0;
+            if(Size.Equals("small"))
+            {
+                cost = 10 + 2 * NoOfCheeseToppings + 2 * NoOfHamToppings + 2 * NoOfPepperoniToppings;
+            }
+            else if(Size.Equals("medium"))
+            {
+                cost = 12 + 2 * NoOfCheeseToppings + 2 * NoOfHamToppings + 2 * NoOfPepperoniToppings;
+            }
+            else if(Size.Equals("large"))
+            {
+                cost = 14 + 2 * NoOfCheeseToppings + 2 * NoOfHamToppings + 2 * NoOfPepperoniToppings;
+            }
+            else
+            {
+                Console.WriteLine("Give correct SIze");
+            }
+            return cost;
+
+        }
+        public void  getDescription()
+        {
+            Console.WriteLine($"PizzaSize: {Size},NoOfCheeseToppings: {NoOfCheeseToppings} ,NoOfPepperoniTopping: {NoOfPepperoniToppings},  NoOfHamToppings : {NoOfHamToppings}");
+
+        }
+
+
+    }
+}
---
+++ Assignment6/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Assignment6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pizza> list = new List<Pizza>();
+            Console.WriteLine("Welcome to Pizza Shop");
+
+            string ch;
+
+            do
+            {
+                Console.WriteLine("Enter Size of Pizza");
+                string size = Console.ReadLine().ToLower();
+
+                Console.WriteLine("Enter number of cheese topping");
+                int cheese = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter number of pepperoni toppings");
+                int pepperoni = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter number of ham topping");
+
+                int ham = int.Parse(Console.ReadLine());
+                Pizza pizza = new Pizza(size, cheese, pepperoni, ham);
+                list.Add(pizza);
+
+                pizza.getDescription();
+                double cost = pizza.calcCost(size);
+                Console.WriteLine("Cost Of Your Pizza:" + cost);
+
+                Console.WriteLine("Do you want to order more(yes/no)");
+                ch= Console.ReadLine().ToLower(); 
+            }
+            while (ch =="yes");
+
+
+
+        }
+    }
+}
